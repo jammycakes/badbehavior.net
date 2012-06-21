ad03429e97ff4c727e537f4d35a135582856ba26
James McKay <code@jamesmckay.net>
2012-06-21 23:22:38 +0100

    Define the core API for the validator.

---
+++ src/BadBehaviour/BadBehaviourModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+
+namespace BadBehaviour
+{
+	public class BadBehaviourModule : IHttpModule
+	{
+		public void Dispose()
+		{
+		}
+
+		public void Init(HttpApplication context)
+		{
+			context.BeginRequest += (sender, e) => {
+				var request = new HttpRequestWrapper(context.Request);
+				Validator.Instance.Validate(request);
+			};
+		}
+	}
+}
---
+++ src/BadBehaviour/Validator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace BadBehaviour
+{
+	public class Validator
+	{
+		public static Validator Instance { get; set; }
+
+		static Validator()
+		{
+			Instance = new Validator();
+		}
+
+		public void Validate(HttpRequestBase request)
+		{
+		}
+	}
+}
