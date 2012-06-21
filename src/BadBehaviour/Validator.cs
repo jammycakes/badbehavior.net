7ddabb67df827504c0458a32243994beb8bdffc9
James McKay <code@jamesmckay.net>
2012-06-21 23:46:56 +0100

    Implement Validator core functionality.

--- src/BadBehaviour/Validator.cs
+++ src/BadBehaviour/Validator.cs
@@ -12,11 +12,36 @@ namespace BadBehaviour
 
 		static Validator()
 		{
-			Instance = new Validator();
+			Instance = new Validator(
+				from type in typeof(Validator).Assembly.GetTypes()
+				where typeof(ITest).IsAssignableFrom(type)
+				let c = type.GetConstructor(Type.EmptyTypes)
+				where c != null
+				let test = c.Invoke(null) as ITest
+				where test != null
+				select test
+			);
+		}
+
+		public IList<ITest> Tests { get; private set; }
+
+		public Validator()
+		{
+			this.Tests = new List<ITest>();
+		}
+
+		public Validator(IEnumerable<ITest> tests)
+		{
+			this.Tests = tests.ToList();
 		}
 
 		public void Validate(HttpRequestBase request)
 		{
+			foreach (var test in this.Tests) {
+				if (test.Test(request)) {
+					throw new BadBehaviourException(test, request);
+				}
+			}
 		}
 	}
 }
