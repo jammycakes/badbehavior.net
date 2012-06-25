3c43ae98ac28a833468dbcc275192fb4c2449c23
James McKay <code@jamesmckay.net>
2012-06-25 18:03:56 +0100

    Declare BadBehaviorEventHandler & args

---
+++ src/BadBehavior/BadBehaviorEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BadBehavior
+{
+    public class BadBehaviorEventArgs : EventArgs
+    {
+        public Error Error { get; private set; }
+
+        public Package Package { get; private set; }
+
+        public BadBehaviorEventArgs(BadBehaviorException ex)
+        {
+            this.Error = ex.Error;
+            this.Package = ex.Package;
+        }
+    }
+
+    public delegate void BadBehaviorEventHandler(object sender, BadBehaviorEventArgs e);
+}
