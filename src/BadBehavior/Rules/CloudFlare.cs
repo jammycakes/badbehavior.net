673ca37d34331d2d9f385033096d4800964b90de
James McKay <code@jamesmckay.net>
2012-06-27 07:40:27 +0100

    Implement stub for CloudFlare test.
    This isn't implemented in BB reference due to a PHP reverse DNS lookup bug.

--- src/BadBehavior/Rules/CloudFlare.cs
+++ src/BadBehavior/Rules/CloudFlare.cs
@@ -9,6 +9,10 @@ namespace BadBehavior.Rules
     {
         public RuleProcessing Validate(Package package)
         {
+            if (package.Headers.ContainsKey("Cf-Connecting-Ip")) {
+                // Not implemented in BB reference.
+            }
+
             return RuleProcessing.Continue;
         }
     }
