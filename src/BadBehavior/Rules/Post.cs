773e218a0d575e03cec61c4d1f510926d4697af3
James McKay <code@jamesmckay.net>
2012-06-28 18:33:40 +0100

    Post tests 1-3 (excluding trackback checks).

--- src/BadBehavior/Rules/Post.cs
+++ src/BadBehavior/Rules/Post.cs
@@ -9,7 +9,36 @@ namespace BadBehavior.Rules
     {
         public RuleProcessing Validate(Package package)
         {
+            if (package.Request.RequestType.Equals("POST", StringComparison.InvariantCultureIgnoreCase)) {
+                VerifyPost(package);
+            }
+
             return RuleProcessing.Continue;
+        }
+
+        private void VerifyTrackbacks(Package package)
+        {
+        }
+
+        private void VerifyPost(Package package)
+        {
+            // MovableType needs specialized screening
+            if (package.UserAgentI.Contains("movabletype")) {
+                if ("bytes=0-99999".Equals(package.HeadersMixed["Range"]))
+                    package.Raise(this, Errors.InvalidRangeHeader);
+            }
+
+            // Trackbacks need special screening
+            if (package.Request.Form.ContainsKey("title") &&
+                package.Request.Form.ContainsKey("url") &&
+                package.Request.Form.ContainsKey("blog_name")) {
+                VerifyTrackbacks(package);
+            }
+
+            // Catch a few completely broken spambots
+            foreach (string key in package.Request.Form.Keys)
+                if (key.Contains("	document.write"))
+                    package.Raise(this, Errors.Malicious);
         }
     }
 }
