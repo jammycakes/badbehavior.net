8cf6be4a899c170365f76a6ebeacaa8fc26ceaa4
James McKay <code@jamesmckay.net>
2012-06-30 10:09:27 +0100

    Implement whitelist.

--- src/BadBehavior/Rules/WhiteList.cs
+++ src/BadBehavior/Rules/WhiteList.cs
@@ -11,6 +11,17 @@ namespace BadBehavior.Rules
         {
             // NB: return RuleProcessing.Stop if this request is whitelisted.
             // Don't throw from this method.
+
+            if (package.OriginatingIP.MatchCidr(package.Configuration.WhitelistIPRanges.ToArray()))
+                return RuleProcessing.Stop;
+
+            if (package.Configuration.WhitelistUserAgents.Any(x => x == package.Request.UserAgent))
+                return RuleProcessing.Stop;
+
+            string url = package.Request.Path;
+            if (package.Configuration.WhitelistUrls.Any(x => url.StartsWith(x)))
+                return RuleProcessing.Stop;
+
             return RuleProcessing.Continue;
         }
     }
