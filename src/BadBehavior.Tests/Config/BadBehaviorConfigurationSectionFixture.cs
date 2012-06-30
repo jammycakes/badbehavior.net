9f0b243471ca284e790b9c75e4c2a53ce9ed6c22
James McKay <code@jamesmckay.net>
2012-06-30 08:50:30 +0100

    Create a new configuration section.
    We will need this for things such as the whitelist and the reverse proxy addresses.

---
+++ src/BadBehavior.Tests/Config/BadBehaviorConfigurationSectionFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using BadBehavior.Config;
+using NUnit.Framework;
+
+namespace BadBehavior.Tests.Config
+{
+    [TestFixture]
+    public class BadBehaviorConfigurationSectionFixture
+    {
+        private BadBehaviorConfigurationSection config;
+
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            this.config = ConfigurationManager.GetSection("badBehavior")
+                as BadBehaviorConfigurationSection;
+        }
+
+        [Test]
+        public void CanLoadConfiguration()
+        {
+            // A couple of quick sanity checks to ensure the config section is loading.
+
+            Assert.IsTrue(config.OffsiteForms);
+            Assert.AreEqual("no-reply@jamesmckay.net", config.SupportEmail);
+            Assert.AreEqual(BadBehaviorConfigurationSection.DefaultReverseProxyHeader,
+                config.ReverseProxyHeader);
+        }
+    }
+}
---
+++ src/BadBehavior/Config/BadBehaviorConfigurationSection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace BadBehavior.Config
+{
+    public class BadBehaviorConfigurationSection : ConfigurationSection, IConfiguration
+    {
+        public const string DefaultReverseProxyHeader = "X-Forwarded-For";
+
+        [ConfigurationProperty("offsiteForms", DefaultValue = false, IsRequired = false)]
+        public bool OffsiteForms
+        {
+            get { return (bool)this["offsiteForms"]; }
+            set { this["offsiteForms"] = value; }
+        }
+
+        [ConfigurationProperty("strict", DefaultValue = false, IsRequired = false)]
+        public bool Strict
+        {
+            get { return (bool)this["strict"]; }
+            set { this["strict"] = value; }
+        }
+
+        [ConfigurationProperty("supportEmail", IsRequired = false)]
+        public string SupportEmail
+        {
+            get { return (string)this["supportEmail"]; }
+            set { this["supportEmail"] = value; }
+        }
+
+        [ConfigurationProperty("reverseProxy", DefaultValue = false, IsRequired = false)]
+        public bool ReverseProxy
+        {
+            get { return (bool)this["reverseProxy"]; }
+            set { this["reverseProxy"] = value; }
+        }
+
+        public IList<string> ReverseProxyAddresses
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        [ConfigurationProperty("reverseProxyHeader", DefaultValue = DefaultReverseProxyHeader, IsRequired = false)]
+        public string ReverseProxyHeader
+        {
+            get { return (string)this["reverseProxyHeader"]; }
+            set { this["reverseProxyHeader"] = value; }
+        }
+    }
+}
