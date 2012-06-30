63623c33d6926d73420653887755966b97b763ab
James McKay <code@jamesmckay.net>
2012-06-30 19:45:43 +0100

    Test for creation of one of the stored procedures.

--- src/BadBehavior.Tests/Logging/SqlServer/DatabaseInstallerFixture.cs
+++ src/BadBehavior.Tests/Logging/SqlServer/DatabaseInstallerFixture.cs
@@ -9,10 +9,14 @@ using NUnit.Framework;
 namespace BadBehavior.Tests.Logging.SqlServer
 {
     [TestFixture]
-    public class DatabaseInstallerFixture
+    public class DatabaseInstallerFixture : Repository
     {
         public static readonly string connectionString =
             ConfigurationManager.ConnectionStrings["BadBehavior"].ConnectionString;
+
+        public DatabaseInstallerFixture()
+            : base(connectionString)
+        { }
 
         [TestFixtureSetUp]
         public void SetupDatabase()
@@ -24,6 +28,14 @@ namespace BadBehavior.Tests.Logging.SqlServer
         [Test]
         public void CanCreateObjects()
         {
+            string s = "select name from dbo.sysobjects where xtype='P'";
+            using (var cn = Connect())
+            using (var cmd = this.GetCommandFromText(cn, s))
+            using (var reader = cmd.ExecuteReader())
+                while (reader.Read())
+                    if (reader.GetString(0) == "BadBehavior_AddEntry") return;
+            Assert.Fail("Stored procedure BadBehavior_AddEntry was not found in the database.");
+
         }
     }
 }
