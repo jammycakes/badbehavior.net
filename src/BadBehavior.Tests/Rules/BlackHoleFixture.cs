4f8e9f6aa625b8e6879d2c3fda04d6c11c98693f
James McKay <code@jamesmckay.net>
2012-07-03 08:07:55 +0100

    BlackHole: Construct DNS name to lookup on blackhole servers.
    $ip = $package['ip'];
    $find = implode('.', array_reverse(explode('.', $ip)));
    
    So for example 1.2.3.4 -> 4.3.2.1.sbl-xbl.spamhaus.org

---
+++ src/BadBehavior.Tests/Rules/BlackHoleFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using BadBehavior.Rules;
+using NUnit.Framework;
+
+namespace BadBehavior.Tests.Rules
+{
+    [TestFixture]
+    public class BlackHoleFixture
+    {
+        [TestCase("127.0.0.1", "sbl-xbl.spamhaus.org", "1.0.0.127.sbl-xbl.spamhaus.org")]
+        public void CanGetDnsblLookup(string ip, string dnsbl, string expected)
+        {
+            string actual = new BlackHole().GetDnsblLookup(IPAddress.Parse(ip), dnsbl);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- src/BadBehavior/Rules/BlackHole.cs
+++ src/BadBehavior/Rules/BlackHole.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace BadBehavior.Rules
 {
     public class BlackHole : IRule
     {
+        // Only conservative lists
+        private static readonly string[] blackholeLists = new string[] {
+            "sbl-xbl.spamhaus.org", // All around nasties
+            // "dnsbl.sorbs.net",      // Old useless data.
+            // "list.dsbl.org",        // Old useless data.
+            // "dnsbl.ioerror.us",     // Bad Behavior Blackhole
+        };
+
+        private static readonly Dictionary<string, string> blackholeExceptions
+            = new Dictionary<string, string>() {
+            { "sbl-xbl.spamhaus.org", "127.0.0.4" },    // CBL is problematic
+            { "dnsbl.sorbs.net", "127.0.0.10" }         // Dynamic IPs only
+        };
+
+        public string GetDnsblLookup(IPAddress ip, string dnsbl)
+        {
+            var bytes = ip.GetAddressBytes().Reverse().ToArray();
+            string s = new IPAddress(bytes).ToString();
+            return s + "." + dnsbl;
+        }
+
         public RuleProcessing Validate(Package package)
         {
+            if (package.OriginatingIP.AddressFamily != AddressFamily.InterNetwork)
+                return RuleProcessing.Continue;
+
+
+
             return RuleProcessing.Continue;
         }
     }
