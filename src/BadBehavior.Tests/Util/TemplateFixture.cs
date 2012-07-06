7fde033c1f14fe1253e475833cdac1deb390b292
James McKay <code@jamesmckay.net>
2012-07-06 07:58:50 +0100

    Add a basic templating engine.
    --HG--
    extra : rebase_source : 44a976f04b509126c87f4e0fe3bfa893b394be8d

---
+++ src/BadBehavior.Tests/Util/TemplateFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BadBehavior.Util;
+using NUnit.Framework;
+
+namespace BadBehavior.Tests.Util
+{
+    [TestFixture]
+    public class TemplateFixture
+    {
+        [Test]
+        public void CanProcessTags()
+        {
+            string sTemplate = "Testing the {{template}} tags";
+            var args = new Dictionary<string, string>() {
+                { "template", "Bad Behavior" }
+            };
+
+            var result = new Template(sTemplate).Process(args);
+            Assert.AreEqual("Testing the Bad Behavior tags", result);
+
+        }
+    }
+}
---
+++ src/BadBehavior/Util/Template.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BadBehavior.Util
+{
+    public class Template
+    {
+        public string Text { get; private set; }
+
+        public Template(string text)
+        {
+            this.Text = text;
+        }
+
+        public static Template FromStream(Stream input)
+        {
+            using (var reader = new StreamReader(input))
+                return new Template(reader.ReadToEnd());
+        }
+
+        internal static Template FromResource(string resourceName)
+        {
+            using (var stream = typeof(Template).Assembly.GetManifestResourceStream(resourceName))
+                return FromStream(stream);
+        }
+
+        private readonly Regex reReplaceTags = new Regex(@"\{\{(.*?)\}\}");
+
+        private string ReplaceTags(Func<string, string> getter)
+        {
+            return reReplaceTags.Replace
+                (this.Text, x => getter(x.Groups[1].Value) ?? String.Empty);
+        }
+
+        public string Process(IDictionary<string, string> data)
+        {
+            Func<string, string> getter = x => {
+                string result;
+                return data.TryGetValue(x, out result) ? result : null;
+            };
+
+            return ReplaceTags(getter);
+        }
+    }
+}
