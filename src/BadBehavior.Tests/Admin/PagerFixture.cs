c3d2983f604024334a01bf41fdfce4f84625ca4c
James McKay <code@jamesmckay.net>
2012-07-18 08:10:51 +0100

    Create Pager class, with method to find the page numbers.

---
+++ src/BadBehavior.Tests/Admin/PagerFixture.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BadBehavior.Admin;
+using BadBehavior.Logging;
+using NUnit.Framework;
+
+namespace BadBehavior.Tests.Admin
+{
+    [TestFixture]
+    public class PagerFixture
+    {
+        [TestCase(1, 10, 1, 2, 3, 0, 10)]
+        [TestCase(2, 10, 1, 2, 3, 4, 0, 10)]
+        [TestCase(3, 10, 1, 2, 3, 4, 5, 0, 10)]
+        [TestCase(4, 10, 1, 2, 3, 4, 5, 6, 0, 10)]
+        [TestCase(5, 10, 1, 0, 3, 4, 5, 6, 7, 0, 10)]
+        [TestCase(6, 10, 1, 0, 4, 5, 6, 7, 8, 0, 10)]
+        [TestCase(7, 10, 1, 0, 5, 6, 7, 8, 9, 10)]
+        [TestCase(8, 10, 1, 0, 6, 7, 8, 9, 10)]
+        [TestCase(9, 10, 1, 0, 7, 8, 9, 10)]
+        [TestCase(10, 10, 1, 0, 8, 9, 10)]
+
+        [TestCase(4, 8, 1, 2, 3, 4, 5, 6, 0, 8)]
+        [TestCase(4, 7, 1, 2, 3, 4, 5, 6, 7)]
+        [TestCase(3, 6, 1, 2, 3, 4, 5, 6)]
+        [TestCase(1, 4, 1, 2, 3, 4)]
+        [TestCase(1, 2, 1, 2)]
+        [TestCase(2, 2, 1, 2)]
+        [TestCase(1, 1, 1)]
+        [TestCase(2, 3, 1, 2, 3)]
+
+        public void CanGetPageNumbers(int pageNumber, int totalPages, params int[] expected)
+        {
+            var query = new LogQuery() {
+                PageNumber = pageNumber
+            };
+            var results = new LogResultSet() {
+                Page = pageNumber,
+                TotalPages = totalPages
+            };
+            var pageNumbers = new Pager(query, results).GetPageNumbers();
+            CollectionAssert.AreEqual(expected, pageNumbers);
+        }
+    }
+}
---
+++ src/BadBehavior/Admin/Pager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BadBehavior.Logging;
+
+
+namespace BadBehavior.Admin
+{
+    public class Pager
+    {
+        private LogQuery baseQuery;
+        private LogResultSet results;
+
+        public Pager(LogQuery baseQuery, LogResultSet results)
+        {
+            this.baseQuery = baseQuery;
+            this.results = results;
+        }
+
+
+        public IEnumerable<int> GetPageNumbers()
+        {
+            yield return 1;
+            if (results.TotalPages > 1) {
+                var firstPage = results.Page - 2;
+                var lastPage = results.Page + 2;
+                if (firstPage < 2) firstPage = 2;
+                if (lastPage > results.TotalPages - 1) lastPage = results.TotalPages - 1;
+                if (firstPage > 2) yield return 0;
+                for (int i = firstPage; i <= lastPage; i++) {
+                    yield return i;
+                }
+                if (lastPage < results.TotalPages - 1) yield return 0;
+                yield return results.TotalPages;
+            }
+        }
+
+
+        public string GetHtml()
+        {
+            if (results.TotalPages == 1) return null;
+
+            return String.Empty;
+        }
+    }
+}
