2cf59288230a41d065ea2f71b7bd626938a4f7fe
James McKay <code@jamesmckay.net>
2012-07-19 21:46:39 +0100

    Document the Package class.

--- src/BadBehavior/Package.cs
+++ src/BadBehavior/Package.cs
@@ -7,8 +7,23 @@ using BadBehavior.Util;
 
 namespace BadBehavior
 {
+    /* ====== Package class ====== */
+
+    /// <summary>
+    ///  Contains a Bad Behavior-packaged version of the HTTP request.
+    /// </summary>
+    /// <remarks>
+    ///  This class is a strongly-typed version of the $package variable
+    ///  that you will see scattered around the original Bad Behavior
+    ///  source code.
+    /// </remarks>
+
     public class Package
     {
+        /// <summary>
+        ///  The <see cref="BBEngine"/> instance that is validating this package.
+        /// </summary>
+
         public BBEngine Engine { get; private set; }
 
         /// <summary>
@@ -44,11 +59,25 @@ namespace BadBehavior
 
         /* ====== Properties computed by the validators ====== */
 
+        /// <summary>
+        ///  Gets or sets a value indicating whether this request is coming from
+        ///  something that claims to be a web browser.
+        /// </summary>
+
         public bool IsBrowser { get; set; }
 
 
-
         /* ====== Constructor ====== */
+
+        /// <summary>
+        ///  Creates a new instance of the <see cref="Package"/> class.
+        /// </summary>
+        /// <param name="request">
+        ///  The HTTP request being validated.
+        /// </param>
+        /// <param name="engine">
+        ///  The <see cref="BBEngine"/> instance performing the validation.
+        /// </param>
 
         public Package(HttpRequestBase request, BBEngine engine)
         {
@@ -88,10 +117,26 @@ namespace BadBehavior
             return Functions.SafeParseIP(Request.UserHostAddress);
         }
 
+        /// <summary>
+        ///  Called when a request has been rejected by a rule.
+        /// </summary>
+        /// <param name="error">
+        ///  The <see cref="Error"/> condition giving details of the violation.
+        /// </param>
+
         public void Raise(Error error)
         {
             this.Engine.Raise(this, error);
         }
+
+        /// <summary>
+        ///  Called when a request has been flagged as suspicious by a rule.
+        ///  The request will be rejected if and only if Bad Behavior is running
+        ///  in strict mode.
+        /// </summary>
+        /// <param name="error">
+        ///  The <see cref="Error"/> condition giving details of the violation.
+        /// </param>
 
         public void RaiseStrict(Error error)
         {
