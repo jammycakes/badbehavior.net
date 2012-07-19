f334d6c677bfd2042c498dbb410e0eb14d1e2b97
James McKay <code@jamesmckay.net>
2012-07-19 21:23:09 +0100

    Document the Error class.

--- src/BadBehavior/Error.cs
+++ src/BadBehavior/Error.cs
@@ -2,6 +2,18 @@
 
 namespace BadBehavior
 {
+    /* ====== Error class ====== */
+
+    /// <summary>
+    ///  Contains generic information about a class of error conditions.
+    /// </summary>
+    /// <remarks>
+    ///  The <see cref="Error"/> class contains generic information that applies
+    ///  to all errors of this class. It includes a support code, the HTTP response
+    ///  code to return, an explanation to display to the user, and an explanation
+    ///  to record in the logs.
+    /// </remarks>
+
     [Serializable]
     public class Error
     {
@@ -29,6 +41,21 @@ namespace BadBehavior
 
         public string Log { get; private set; }
 
+        /// <summary>
+        ///  Creates a new instance of the <see cref="Error"/> class.
+        /// </summary>
+        /// <param name="code">
+        ///  The Bad Behavior support code.
+        /// </param>
+        /// <param name="httpCode">
+        ///  The HTTP response code.
+        /// </param>
+        /// <param name="explanation">
+        ///  The explanation to show to the user.
+        /// </param>
+        /// <param name="log">
+        ///  The message to show in the logs.
+        /// </param>
 
         public Error(string code, int httpCode, string explanation, string log)
         {
