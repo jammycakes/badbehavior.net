e7edf0b02500b4456ddb003def3af3473ea216c7
James McKay <code@jamesmckay.net>
2012-07-23 07:48:47 +0100

    Replace old BadBehaviorSettings with new BadBehaviorConfigurator.

---
+++ src/Website/BadBehaviorConfigurator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BadBehavior;
+
+namespace Website
+{
+    public class BadBehaviorConfigurator : IConfigurator
+    {
+        public void Configure(BBEngine engine)
+        {
+            var settings = engine.Settings;
+            settings.Debug = true;
+        }
+    }
+}
